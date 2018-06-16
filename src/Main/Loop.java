ff1f82947feb0b163d75082cb9f3556445971762
GodOfBraze <RomanovAlexanderDmitrievich@gmail.com>
2018-06-16 14:06:24 +0800

    fuck
    fuck

--- src/Main/Loop.java
+++ src/Main/Loop.java
@@ -3,12 +3,14 @@ package Main;
 public class Loop implements Runnable{
     private int counter;
     private ProgressApp app;
+    private boolean started = false;
 
     Loop(ProgressApp app){
         this.app = app;
     }
 
     private void startLoop() throws InterruptedException {
+        //while (!app.isStart){}
         while (counter < 1000){
             counter++;
             app.progressBar.setValue(counter);
@@ -19,6 +21,15 @@ public class Loop implements Runnable{
 
     @Override
     public void run() {
+        while (!started){
+            started = app.isStart;
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         try {
             startLoop();
         } catch (InterruptedException e) {
